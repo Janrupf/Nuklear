a498d957fe20441b12eafdc9b0112020409316a3
janrupf <business.janrupf@gmail.com>
2020-07-06 14:49:51 +0200

    Move border drawing to nk_panel_end

--- nuklear.h
+++ nuklear.h
@@ -19485,10 +19485,7 @@ nk_panel_begin(struct nk_context *ctx, const char *title, enum nk_panel_type pan
         body.h = (win->bounds.h - layout->header_height);
         if (style->window.fixed_background.type == NK_STYLE_ITEM_IMAGE)
             nk_draw_image(out, body, &style->window.fixed_background.data.image, nk_white);
-        else {
-            nk_fill_rect(out, body, style->window.rounding, style->window.fixed_background.data.color);
-            nk_stroke_rect(out, body, style->window.rounding, nk_panel_get_border(style, win->flags, panel_type), nk_panel_get_border_color(style, panel_type));
-        }
+        else nk_fill_rect(out, body, style->window.rounding, style->window.fixed_background.data.color);
     }
 
     /* set clipping rectangle */
@@ -19682,7 +19679,7 @@ nk_panel_end(struct nk_context *ctx)
                 : (window->bounds.y + window->bounds.h));
         struct nk_rect b = window->bounds;
         b.h = padding_y - window->bounds.y;
-        nk_stroke_rect(out, b, 0, layout->border, border_color);
+        nk_stroke_rect(out, b, style->window.rounding, nk_panel_get_border(style, layout->flags, layout->type), nk_panel_get_border_color(style, layout->type));
     }
 
     /* scaler */
